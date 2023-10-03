5987b86379b3c57eb91ad1771f7d306df4088a25
Perkles <deivyssonlucas30@gmail.com>
2023-10-03 10:15:30 -0300

    Criando setup inicial e component header

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/header"
+//import styles from '../styles/Home.module.scss'
+import styles from '../styles/global.scss'
+export default function Home(){
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  )
+}
